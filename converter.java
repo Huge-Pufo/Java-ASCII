07abc6bb2ef2f5fe3a5d899dcf3339869ed404fd
Huge-Pufo <einsteinfilho63@gmail.com>
2025-12-10 08:44:05 +0000

    loop

--- converter.java
+++ converter.java
@@ -1,8 +1,9 @@
-import java.net.URL;
 import java.awt.image.BufferedImage;
-import java.util.scanner;
+import javax.imageio.ImageIO;
+import java.net.URL;
+import java.util.Scanner;
 
-public class AsciiImages
+public class AsciiImage
 {
     public static void main(String[] args)
     {
@@ -11,7 +12,26 @@ public class AsciiImages
         //Takes image URL and stores it in image_raster
         System.out.println("Provide an image URL"); 
         String image_url = scan.nextLine();
+        image_url = image_url.trim();
         
-        BufferedImage image_raster = ImageIO.read(new URL(image_url));
+        BufferedImage image = ImageIO.read(new URL(image_url));
+
+        String ascii_image = "";
+        for (int y = image.getMinY(); y < image.getHeight() - 4; y+=4)
+        {
+            for (int x = image.getMinX(); x < image.getWidth() - 4; x+=4)
+            {
+                for (int row = 0; row < 4; row++)
+                {
+                    int brightness = 0;
+                    for(int col = 0; col < 4; col++)
+                    {
+                        brightness += image.get(x + row, y + col);
+                    }
+                }
+            }
+        }
+
+
     }
 }
